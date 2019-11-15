e069b3264d731326168c8089b7371f8a4af7907e
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-11-15 12:11:43 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const array = ['Lisa', 'Kaitlin', 'Jan']
+const word = "surprise"
+const newArray = [];
+
+function writeCards(array, word) {
+    for (let i = 0; i < array.length; i++) {
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${word} gift!`;
+    }
+    return newArray
+}
+
+writeCards(array, word);
+
+
+
+function countDown() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+countDown();
